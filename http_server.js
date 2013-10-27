fc16e5e63f90663b91adf6ac1947c6872f88f61b
huiyinchu <huiyinchu@gmail.com>
2013-10-27 14:57:49 -0700

    fix the counter increment bug

--- http_server.js
+++ http_server.js
@@ -3,10 +3,11 @@
 var http = require('http');
 var counter = 0;
 var serv = http.createServer(function(req,res) {
-    counter += 1;
-    res.writeHead(200);
-    debugger;
-    res.end("Cumulative number of requests: " + counter);
+    if(req.url != "/favicon.ico"){
+	counter += 1;
+    	res.writeHead(200);
+    	res.end("Cumulative number of requests: " + counter);
+    }
 });
 var port = 8080;
 serv.listen(port);
